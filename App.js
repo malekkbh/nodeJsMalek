85dc7c54bc65d196bf47f90cfcde4e58adc231ee
malekqs <malek.kbh@gmail.com>
2023-05-20 17:10:39 +0300

    ..

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const StudentModule = require("./modules/student.module");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-//get req
-
-const mongooseLink = "mongodb+srv://malek:123456780@test.c38nkyc.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(mongooseLink) ; 
-mongoose.connection.on('connected' , ()=> {
-    console.log("mongo connected");
-})
+const mongooseLink =
+  "mongodb+srv://malek:123456780@test.c38nkyc.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongooseLink);
+mongoose.connection.on("connected", () => {
+  console.log("mongo connected");
+});
 
 app.get("/app", (req, res) => {
   res.status(200).json({
     message: "yes",
+    batat: "10kg",
   });
 });
 
-module.exports = app; 
+app.post("/creatNewStudent", (req, res) => {
+  StudentModule.create({
+    name: req.body.name,
+    id: req.body.id,
+  }).then((response) => {
+    res.status(200).json({
+      message: "done",
+    });
+  });
+});
+
+app.get("/getAllUsers",  (req, res) => {
+  
+  // try {
+  //   const allUsers = await StudentModule.find();
+  // } catch (error) {
+  //   console.log("get all students error: ", e);
+  // }
+
+  StudentModule.find()
+    .then((stRes) => {
+      console.log("");
+      res.status(200).json({
+        message: "done",
+        users: stRes,
+      });
+    })
+    .catch((e) => {
+      console.log("get all students error: ", e);
+    });
+});
+
+module.exports = app;
